124a1d14e62fa135436f42368a46e5b2b14efd23
Yanick <moussoundayannick89@gmail.com>
2022-09-02 06:12:37 +0100

    💄 UI:  past code an other branch from github

--- src/components/view/log.vue
+++ src/components/view/log.vue
@@ -1,103 +1,123 @@
 <template>
-  <div>
-    <!-- Inscription -->
-    <div v-if="showModalSignUp" :class="{isActive}">
-      <div class="log">
-        <button class="close-icon" @click="showModalSignUp= false">
-          <svg width="16px" height="16px" viewBox="0 0 16 16" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg">
-            <defs>
-              <path d="M0 0L16 0L16 16L0 16L0 0Z" id="path_1" />
-              <clipPath id="mask_1">
-                <use xlink:href="#path_1" />
-              </clipPath>
-            </defs>
-            <g id="Delete-icon">
-            <path d="M0 0L16 0L16 16L0 16L0 0Z" id="Background" fill="none" fill-rule="evenodd" stroke="none" />
-            <g clip-path="url(#mask_1)">
-              <path d="M2.75 2.04297L2.04297 2.75L2.39844 3.10156L7.29297 8L2.04297 13.25L2.75 13.957L8 8.70703L12.8945 13.6055L13.25 13.957L13.957 13.25L13.6055 12.8945L8.70703 8L13.957 2.75L13.25 2.04297L8 7.29297L3.10156 2.39844L2.75 2.04297Z" id="Shape" fill="#111111" stroke="none" />
-            </g>
-            </g>
-          </svg>
-        </button>
-        <div class="formulaire">
-          <form action="">
+  <div class="main-content">
+    <form>
+      <!-- Inscription -->
+      <div v-if="showModalSignUp" :class="{isActive}">
+        <div class="log">
+          <button class="close-icon" @click="showModalSignUp= false">
+            <svg width="16px" height="16px" viewBox="0 0 16 16" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg">
+              <defs>
+                <path d="M0 0L16 0L16 16L0 16L0 0Z" id="path_1" />
+                <clipPath id="mask_1">
+                  <use xlink:href="#path_1" />
+                </clipPath>
+              </defs>
+              <g id="Delete-icon">
+              <path d="M0 0L16 0L16 16L0 16L0 0Z" id="Background" fill="none" fill-rule="evenodd" stroke="none" />
+              <g clip-path="url(#mask_1)">
+                <path d="M2.75 2.04297L2.04297 2.75L2.39844 3.10156L7.29297 8L2.04297 13.25L2.75 13.957L8 8.70703L12.8945 13.6055L13.25 13.957L13.957 13.25L13.6055 12.8945L8.70703 8L13.957 2.75L13.25 2.04297L8 7.29297L3.10156 2.39844L2.75 2.04297Z" id="Shape" fill="#111111" stroke="none" />
+              </g>
+              </g>
+            </svg>
+          </button>
+          <div class="formulaire">         
             <label for="name">
               Nom:
-              <input type="text" name="" id="name" placeholder="nom">
+              <input type="text" v-model="createUser.name" id="name" placeholder="Nom">
+            </label>
+            <label for="subname">
+              Prénom:
+              <input type="text" v-model="createUser.lastName" id="subname" placeholder="Prénom">
             </label>
             <label for="mail">
               Email:
-              <input type="email" name="" id="mail" placeholder="mouzx@y.com">
+              <input type="email" v-model="createUser.email" id="mail" placeholder="mouzx@y.com">
             </label>
             <label for="password">
               Mot de passe:
-              <input type="password" name="" id="password" placeholder="mot de passe">
+              <input type="password"  v-model="createUser.passWord" id="password" placeholder="mot de passe">
             </label>
             <div class="submit">
-              <button type="submit"> Enregister</button>
-            </div>
-          </form>
-        </div>
-        <div class="description">
-          <p>Vous avez déjà un compte? <span @click="openLinks">Incrivez-vous</span></p>
+              <button type="submit" @click.prevent="signUp()"> Enregister</button>
+            </div>        
+          </div>
+          <div class="description">
+            <p>Vous avez déjà un compte? <span @click="openLinks">Incrivez-vous</span></p>
+          </div>
         </div>
       </div>
-    </div>
-    <!-- Connexion -->
-    <div v-if="showModalSignIn" :class="{isActive}">
-      <div class="log">
-        <button class="close-icon" @click="showModalSignIn= false">
-          <svg width="16px" height="16px" viewBox="0 0 16 16" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg">
-            <defs>
-              <path d="M0 0L16 0L16 16L0 16L0 0Z" id="path_1" />
-              <clipPath id="mask_1">
-                <use xlink:href="#path_1" />
-              </clipPath>
-            </defs>
-            <g id="Delete-icon">
-            <path d="M0 0L16 0L16 16L0 16L0 0Z" id="Background" fill="none" fill-rule="evenodd" stroke="none" />
-            <g clip-path="url(#mask_1)">
-              <path d="M2.75 2.04297L2.04297 2.75L2.39844 3.10156L7.29297 8L2.04297 13.25L2.75 13.957L8 8.70703L12.8945 13.6055L13.25 13.957L13.957 13.25L13.6055 12.8945L8.70703 8L13.957 2.75L13.25 2.04297L8 7.29297L3.10156 2.39844L2.75 2.04297Z" id="Shape" fill="#111111" stroke="none" />
-            </g>
-            </g>
-          </svg>
-        </button>
-        <div class="formulaire">
-          <form action="">
+      <!-- Connexion -->
+      <div v-if="showModalSignIn" :class="{isActive}">
+        <div class="log">
+          <button class="close-icon" @click="showModalSignIn= false">
+            <svg width="16px" height="16px" viewBox="0 0 16 16" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg">
+              <defs>
+                <path d="M0 0L16 0L16 16L0 16L0 0Z" id="path_1" />
+                <clipPath id="mask_1">
+                  <use xlink:href="#path_1" />
+                </clipPath>
+              </defs>
+              <g id="Delete-icon">
+              <path d="M0 0L16 0L16 16L0 16L0 0Z" id="Background" fill="none" fill-rule="evenodd" stroke="none" />
+              <g clip-path="url(#mask_1)">
+                <path d="M2.75 2.04297L2.04297 2.75L2.39844 3.10156L7.29297 8L2.04297 13.25L2.75 13.957L8 8.70703L12.8945 13.6055L13.25 13.957L13.957 13.25L13.6055 12.8945L8.70703 8L13.957 2.75L13.25 2.04297L8 7.29297L3.10156 2.39844L2.75 2.04297Z" id="Shape" fill="#111111" stroke="none" />
+              </g>
+              </g>
+            </svg>
+          </button>
+          <div class="formulaire">         
             <label for="mail">
               Email:
-              <input type="email" name="" id="mail" placeholder="mouzx@y.com">
+              <input type="email" v-model="connectUser.email" id="mail" placeholder="mouzx@y.com" autocomplete="email">
             </label>
             <label for="password">
               Mot de passe:
-              <input type="password" name="" id="password" placeholder="mot de passe">
+              <input type="password" v-model="connectUser.passWord" id="password" placeholder="mot de passe" autocomplete="current-password">
             </label>
             <div class="submit">
-              <button type="submit"> Connexion</button>
-            </div>
-          </form>
-        </div>
-        <div class="description">
-          <p>Vous n'avez pas de compte? <span @click="openLinkConnexion">Cliquez-vous ici</span></p>
+              <button type="submit" @click.prevent="signIn"> Connexion</button>
+            </div>         
+          </div>
+          <div class="description">
+            <p>Vous n'avez pas de compte? <span @click="openLinkConnexion">Cliquez-vous ici</span></p>
+          </div>
         </div>
       </div>
-    </div>
+    </form>
+    <!-- <loader ref="loader" /> -->
   </div>
 </template>
 
 <script>
+// import loader from '../loader'
+import firbase from '../../firebase';
+import {auth, db} from '../../firebase';
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile   } from "firebase/auth";
+import { collection, doc, setDoc, addDoc} from "firebase/firestore"; 
 export default {
   name: 'logUser',
+  // components:{loader},
   data () {
     return {
       showModalSignUp: false,
       showModalSignIn: false,
-      isActive: true
+      isActive: true,
+      createUser:{
+        name: '',
+        lastName: '',
+        email:'',
+        passWord:'',
+        photoUrl: ''
+      },
+      connectUser:{
+        email:'',
+        passWord:''
+      }
     }
   },
   methods:{
     openLinks(){
-      if(this.showModalSignUp = true){
+      if(this.showModalSignUpfalse){
         this.showModalSignUp = false,
         this.showModalSignIn = true        
         console.log('Inscription ');
@@ -110,39 +130,75 @@ export default {
         console.log('Connexion ');
       } 
     },
+    // New user created
+    signUp(){
+      createUserWithEmailAndPassword(auth, this.createUser.email, this.createUser.passWord )    
+      .then( async (response) =>{
+        const user = response.user;
+        const userId = await setDoc(doc(db, 'users', user.uid),{
+          name: this.createUser.name,
+          lastName: this.createUser.lastName,
+          email: this.createUser.email,
+          uid: user.uid,
+          photoUrl: '',
+          pseudo: '',
+          pseudoName: '',
+          activity: '',
+          yourLinkWeb: ''
+        })
+        // const user = response.user;
+        console.log('displayName and userId', user, ' docId ', userId);
+        updateProfile(user, { displayName: this.createUser.name + ' ' + this.createUser.lastName });
+        this.$router.push('/components/view/profile/userProfile')
+        this.showModalSignUp = false
+      })     
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log('erreur ', errorCode, errorMessage);
+      });
+    },
+    // Connexion  User
+    signIn(){
+      signInWithEmailAndPassword(auth, this.connectUser.email, this.connectUser.passWord)
+      .then(response =>{
+        this.$router.push('/components/view/profile/userProfile')
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+    }
+    },
+    
   }
-}
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+ 
   .isActive{
     position: fixed;  
     width: 350px;
-    background: rgb(255, 255, 255);
+    background: var(--background-white-primary);
     border-radius: 15px;
     padding: 20px;
-    z-index: 9999;
-    box-shadow: 0 0 10px #212121e0, 100px 100px 100px 9000em rgb(48 47 47 / 74%);
-    opacity: 0;
+    z-index: 20;
+    box-shadow: 0 0 10px var( --shadow-color-primary-light), 100px 100px 100px 9000em rgb(48 47 47 / 74%);
+    transform: translateY(-150%);
     animation-name: appear;
-    animation-duration: .5s;
-    animation-timing-function: cubic-bezier(0.45, 0.04, 0.69, 1.15);
+    animation-duration: .3s;
     animation-delay: .1s;
     animation-duration: alternate;
     animation-fill-mode: forwards;
-    transition: ease-out 3s;
   }
 
   @keyframes appear {
     0%{
       transform: translate(150%, -90%);
-      /* opacity: .5;  */
-      /* transform: scale(0); */
     }
     100%{
-      opacity: 1;
-      /* transform: scale(1); */
       transform: translate(150%, 10%);
     }
   }
@@ -162,7 +218,7 @@ export default {
     font-size: 16px;
     border-radius: 5px;
     outline: none;
-    background: #e6e6e6;
+    background: var(--background-input-purple-thin);
     border: none;
   }
   button{
@@ -183,8 +239,8 @@ export default {
     width: 32px;
   }
   .formulaire button{
-    background: #663399;
-    color: #fff;
+    background: var(--background-purple-color);
+    color: var( --text-white-primary);
     width: 100%;
     margin: 0 0 15px 0;
     padding: 10px 5px;
@@ -193,7 +249,7 @@ export default {
   .description span{
     font-size: 13px;
     font-weight: 700;
-    color: #663399;
+    color: var(--text-purple-color);
     cursor: pointer;
   }
 
