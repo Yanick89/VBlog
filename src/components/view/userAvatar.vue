d15f4197aecb240e6abfd7badafd21edce8052ac
Yanick <moussoundayannick89@gmail.com>
2022-09-04 11:38:15 +0100

    👌 Code review : clean, change & review code

--- src/components/view/userAvatar.vue
+++ src/components/view/userAvatar.vue
@@ -1,20 +1,12 @@
 <template>
  <div>
-    <span>
-      <svg width="32px" height="32px" viewBox="0 0 16 16" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg">
-        <defs>
-          <path d="M0 0L16 0L16 16L0 16L0 0Z" id="path_1" />
-          <clipPath id="mask_1">
-            <use xlink:href="#path_1" />
-          </clipPath>
-        </defs>
-        <g id="Contacts-icon">
-          <path d="M0 0L16 0L16 16L0 16L0 0Z" id="Background" fill="none" fill-rule="evenodd" stroke="none" />
-          <g clip-path="url(#mask_1)">
-            <path d="M8 2C6.34766 2 5 3.34766 5 5C5 6.65234 6.34766 8 8 8C9.65234 8 11 6.65234 11 5C11 3.34766 9.65234 2 8 2L8 2ZM8 8C5.24609 8 3 10.2461 3 13L4 13C4 10.7852 5.78516 9 8 9C10.2148 9 12 10.7852 12 13L13 13C13 10.2461 10.7539 8 8 8L8 8ZM8 3C9.10938 3 10 3.89063 10 5C10 6.10938 9.10938 7 8 7C6.89063 7 6 6.10938 6 5C6 3.89063 6.89063 3 8 3L8 3Z" id="Shape" fill="#FFFFFF" fill-rule="evenodd" stroke="none" />
-          </g>
-        </g>
-      </svg>
+    <div class="img-profile" v-if="userInfos.profilPhoto">
+      <img :src="userInfos.profilPhoto" alt="profil photo">
+    </div>
+    <span v-else>
+      <h3 class="img-letter">
+        {{ firstLetter + '' +  secondLetter}}
+      </h3>
     </span>
  </div>
 </template>
@@ -22,6 +14,19 @@
 <script>
 export default {
   name: 'user',
+  props:{
+      firstLetter:{
+        default: null,
+        type: String
+      },
+      secondLetter:{
+        default: null,
+        type: String
+      },
+      userInfos:{
+        default: {}
+      }
+  },
   data () {
     return {
       // msg: "Bienvenue dans l'Appli Vue.js"
@@ -30,7 +35,3 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-    
-</style>
